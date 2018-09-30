aacb4ddd2a89a395fcd2abdea5d4beea12319974
Egor Gorbik <gorbikega@gmail.com>
2018-09-30 12:50:43 +0300

    add Money-Exchange task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-// PLEASE DON'T change function name
+
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if(currency <= 0) return {};
+    if(currency > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    var result = {};
+    var H = Math.floor(currency / 50);
+    if(H > 0) result.H = H;
+    var currency = currency % 50;
+
+    var Q = Math.floor(currency / 25);
+    if(Q > 0) result.Q = Q;
+    var currency = currency % 25;
+
+    var D = Math.floor(currency / 10);
+    if(D > 0) result.D = D;
+    var currency = currency % 10;
+
+    var N = Math.floor(currency / 5);
+    if(N > 0) result.N = N;
+    var currency = currency % 5;
+
+    var P = Math.floor(currency / 1);
+    if(P > 0) result.P = P;
+    var currency = currency % 1;
+
+    return result;
 }
